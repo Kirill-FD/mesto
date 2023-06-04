c97adf5373e008239ce6b465a9540f8f791a5d9c
Kirbro36 <120019115+Kirbro36@users.noreply.github.com>
2023-06-04 19:14:56 +0300

    Add files via upload

---
+++ src/scripts/components/UserInfo.js
@@ -0,0 +1,20 @@
+export default class UserInfo {
+    constructor(inputNameSelector, inputAboutSelector, inputAvatar) {
+        this._inputName = document.querySelector(inputNameSelector);
+        this._inputAbout = document.querySelector(inputAboutSelector);
+        this._inputAvatar = document.querySelector(inputAvatar);
+    }
+
+    getUserInfo() {
+        return {
+            name: this._inputName.textContent,
+            about: this._inputAbout.textContent
+        }
+    }
+
+    setUserInfo({ avatar, name, about }) {
+        this._inputAvatar.src = avatar
+        this._inputName.textContent = name
+        this._inputAbout.textContent = about
+    }
+}
